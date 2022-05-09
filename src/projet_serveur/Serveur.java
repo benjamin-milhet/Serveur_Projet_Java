de72aa11020b65d237991d59b3d2bfe2c6f01f4e
Benjamin Milhet <66473784+Orchanyne@users.noreply.github.com>
2022-05-09 18:05:48 +0200

    Serveur + client handler

--- src/projet_serveur/Serveur.java
+++ src/projet_serveur/Serveur.java
@@ -5,14 +5,43 @@ import java.io.*;
 
 public class Serveur {
 
+    private ServerSocket serverSocket;
+    private  Socket socket;
+
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
     public Serveur() throws IOException {
-        ServerSocket socket = new ServerSocket(8080);
-        while (true) {
-            Socket client = socket.accept();
-            System.out.println(client.getInetAddress().getHostAddress());
-            System.out.println("Client connecté");
-            //new Thread(new ServeurThread(client)).start();
+        this.serverSocket = new ServerSocket(8080);
+
+        try {
+
+            while (!serverSocket.isClosed()) {
+                // Will be closed in the Client Handler.
+                this.socket = serverSocket.accept();
+                System.out.println("A new client has connected!");
+
+                ClientHandler clientHandler = new ClientHandler(socket);
+                Thread thread = new Thread(clientHandler);
+                // The start method begins the execution of a thread.
+                // When you call start() the run method is called.
+                // The operating system schedules the threads.
+                thread.start();
+            }
+
+        } catch (IOException e) {
+            closeServerSocket();
         }
     }
-}
 
+    public void closeServerSocket() {
+        try {
+            if (this.serverSocket != null) {
+                this.serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
