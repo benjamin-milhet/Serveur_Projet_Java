1c0164a38fef84bab5f42b1122687dc1bb4f7643
Benjamin Milhet <66473784+Orchanyne@users.noreply.github.com>
2022-05-10 14:43:14 +0200

    clean code

--- src/projet_serveur/Serveur.java
+++ src/projet_serveur/Serveur.java
@@ -8,23 +8,17 @@ public class Serveur {
     private ServerSocket serverSocket;
     private  Socket socket;
 
-    private BufferedReader bufferedReader;
-    private BufferedWriter bufferedWriter;
     public Serveur() throws IOException {
         this.serverSocket = new ServerSocket(8080);
 
         try {
 
             while (!serverSocket.isClosed()) {
-                // Will be closed in the Client Handler.
                 this.socket = serverSocket.accept();
                 System.out.println("A new client has connected!");
 
                 ClientHandler clientHandler = new ClientHandler(socket);
                 Thread thread = new Thread(clientHandler);
-                // The start method begins the execution of a thread.
-                // When you call start() the run method is called.
-                // The operating system schedules the threads.
                 thread.start();
             }
 
